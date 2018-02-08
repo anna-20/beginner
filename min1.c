fd44db2a2253f2d46d73cc4ca7f847b2cc8885fa
anna-20 <anna1997.bots@gmail.com>
2018-02-08 15:15:29 +0530

    added min1.c

---
+++ min1.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main(void)
+{
+	int t,n,m,i,j,k,rs=5,r,min=0;
+	scanf("%d",&t);
+	if(t>=1&&t<=200)
+	{
+		for (k = 0; k <t; ++k)
+		{
+			
+		
+		scanf("%d%d",&n,&m);
+		if(n>=1&&m>=1&&n<=100&&m<=100)
+		{
+			int A[n][m];
+			for (i = 0; i < n; ++i)
+			{
+				for ( j = 0; j < m; ++j)
+				{
+					scanf("%d",&A[i][j]);
+					if(A[i][j]==1)
+						++min;
+				}
+				if(min<rs)
+				{
+					rs=min;
+					r=i;
+					min=0;
+				}
+			}
+		}
+		if (rs==0)
+	
+		printf("-1\n");
+	else
+	printf("%d\n", r);
+     rs=5;
+     min=0;
+ }
+	}
+	
+return 0;
+}
